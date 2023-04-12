e55a3de9d4c4f65ddf8778e3845ba55cb7a5d72c
Laura Augusta <laurah.algusta@gmail.com>
2023-04-12 15:23:11 -0300

    Item component

--- src/components/Tabela/index.jsx
+++ src/components/Tabela/index.jsx
@@ -1,34 +1,10 @@
+import Item from "../TableItem";
+
 export default function Table() {
     return (
         <div className="container mx-auto px-60">
             <div className="flex flex-col">
                 <div className="overflow-x-auto">
-                    <div className="py-3 pl-2">
-                        <div className="relative max-w-xs">
-
-                            {/* <input
-                                type="text"
-                                name="hs-table-search"
-                                id="hs-table-search"
-                                className="block w-full p-3 pl-10 text-sm border-gray-200 rounded-md focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400"
-                            /> */}
-
-                            {/* <div className="absolute inset-y-0 left-0 flex items-center pl-4 pointer-events-none">
-                                <svg
-                                    className="h-3.5 w-3.5 text-gray-400"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="16"
-                                    height="16"
-                                    fill="currentColor"
-                                    viewBox="0 0 16 16"
-                                >
-                                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-                                </svg>
-                            </div> */}
-
-                        </div>
-                    </div>
-
                     <div className="p-1.5 w-full inline-block align-middle">
                         <div className="overflow-hidden border rounded-lg">
                             <table className="min-w-full divide-y divide-gray-200">
@@ -37,122 +13,16 @@ export default function Table() {
                                         <th scope="col" className="py-3 pl-4">
                                             <div className="flex items-center h-5"></div>
                                         </th>
-                                        <th
-                                            scope="col"
-                                            className="px-6 py-3 text-xs font-bold text-left text-gray-500 uppercase "
-                                        >
-                                            ID
-                                        </th>
-                                        <th
-                                            scope="col"
-                                            className="px-6 py-3 text-xs font-bold text-left text-gray-500 uppercase "
-                                        >
-                                            Name
-                                        </th>
-                                        <th
-                                            scope="col"
-                                            className="px-6 py-3 text-xs font-bold text-left text-gray-500 uppercase "
-                                        >
-                                            Email
-                                        </th>
-                                        <th
-                                            scope="col"
-                                            className="px-6 py-3 text-xs font-bold text-left text-gray-500 uppercase "
-                                        >
-                                            Phone
-                                        </th>
-                                        
+                                        <th scope="col" className="px-6 py-3 text-xs font-bold text-left text-gray-500 uppercase ">ID</th>
+                                        <th scope="col" className="px-6 py-3 text-xs font-bold text-left text-gray-500 uppercase ">Name</th>
+                                        <th scope="col" className="px-6 py-3 text-xs font-bold text-left text-gray-500 uppercase ">Email</th>
+                                        <th scope="col" className="px-6 py-3 text-xs font-bold text-left text-gray-500 uppercase ">Phone</th>
                                     </tr>
                                 </thead>
                                 <tbody className="divide-y divide-gray-200">
-                                    <tr>
-                                        <td className="py-3 pl-4">
-                                            <div className="flex items-center h-5">
-                                                <input
-                                                    type="checkbox"
-                                                    className="text-blue-600 border-gray-200 rounded focus:ring-blue-500"
-                                                />
-                                                <label
-                                                    htmlFor="checkbox"
-                                                    className="sr-only"
-                                                >
-                                                    Checkbox
-                                                </label>
-                                            </div>
-                                        </td>
-                                        <td className="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap">
-                                            1
-                                        </td>
-                                        <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
-                                            Maria Doe
-                                        </td>
-                                        <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
-                                            jonne62@gmail.com
-                                        </td>
-                                        <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
-                                            (99) 94899-9999
-                                        </td>
-                                        <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
-                                            <a
-                                                className="text-green-500 hover:text-green-700"
-                                                href="/"
-                                            >
-                                                Edit
-                                            </a>
-                                        </td>
-                                        <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
-                                            <a
-                                                className="text-red-500 hover:text-red-700"
-                                                href="/"
-                                            >
-                                                Delete
-                                            </a>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td className="py-3 pl-4">
-                                            <div className="flex items-center h-5">
-                                                <input
-                                                    type="checkbox"
-                                                    className="text-blue-600 border-gray-200 rounded focus:ring-blue-500"
-                                                />
-                                                <label
-                                                    htmlFor="checkbox"
-                                                    className="sr-only"
-                                                >
-                                                    Checkbox
-                                                </label>
-                                            </div>
-                                        </td>
-                                        <td className="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap">
-                                            1
-                                        </td>
-                                        <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
-                                            Jone Doe dos sanros
-                                        </td>
-                                        <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
-                                            jonne62@gmail.com
-                                        </td>
-                                        <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
-                                            (99) 94899-9999
-                                        </td>
-                                        <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
-                                            <a
-                                                className="text-green-500 hover:text-green-700"
-                                                href="/"
-                                            >
-                                                Edit
-                                            </a>
-                                        </td>
-                                        <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
-                                            <a
-                                                className="text-red-500 hover:text-red-700"
-                                                href="/"
-                                            >
-                                                Delete
-                                            </a>
-                                        </td>
-                                    </tr>
+                                    {/* =============================================================================== */}
+                                    <Item />
+                                    {/* =============================================================================== */}
                                 </tbody>
                             </table>
                         </div>
---
+++ src/components/TableItem/index.jsx
@@ -0,0 +1,26 @@
+function Item(){
+    return(
+        <>
+            <tr>
+                <td className="py-3 pl-4">
+                    <div className="flex items-center h-5">
+                        <input type="checkbox" className="text-blue-600 border-gray-200 rounded focus:ring-blue-500"/>
+                        <label htmlFor="checkbox" className="sr-only">Checkbox</label>
+                    </div>
+                </td>
+                <td className="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap">1</td>
+                <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">Maria Doe</td>
+                <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">jonne62@gmail.com</td>
+                <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">(99) 94899-9999</td>
+                <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
+                    <a className="text-green-500 hover:text-green-700" href="/"> Edit</a>
+                </td>
+                <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
+                    <a className="text-red-500 hover:text-red-700" href="/"> Delete </a>
+                </td>
+            </tr>
+        </>
+    )
+}
+
+export default Item
